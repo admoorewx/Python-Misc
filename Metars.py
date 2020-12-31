27230ee57255d9865f3040794e1a6d674f5bcba5
Andrew Moore <41948559+admoorewx@users.noreply.github.com>
2020-12-31 17:14:32 -0600

    METAR is ready?
    - Changed default missing type to nan (from 9999)
    - created plot_metar meteograms

--- Metars.py
+++ Metars.py
@@ -14,8 +14,8 @@ TO DO:
 """
 
 
-start = time.time()
-sites = ["KOKC", "KOUN", "KGFK"]
+# start = time.time()
+# sites = ["KOKC", "KOUN", "KGFK"]
 
 
 class METAR:
@@ -31,9 +31,10 @@ class METAR:
         self.gusts = []
         self.wx    = []
         self.press = []
-        self.clouds = []
+        self.cloudCover = []
         self.ceilings = []
         self.vis    = []
+        self.precip = []
         self.rawObs = []
 
 
@@ -66,15 +67,86 @@ class METAR:
         self.press.append(pres)
     def addVis(self,visby):
         self.vis.append(visby)
-    def addClouds(self,cloudcover):
-        self.clouds.append(cloudcover)
-    def addCeilings(self,ceilString):
-        self.ceilings.append(ceilString)
+    def addCloudCover(self,cloudcovers):
+        self.cloudCover.append(cloudcovers)
+    def addCeilings(self,ceilings):
+        self.ceilings.append(ceilings)
+    def addPrecip(self,precipitation):
+        self.precip.append(precipitation)
+
+
+##################################################################################
 
 
 
 
-
+def parseAWCxml(metar,child):
+    time = child.findtext('observation_time')
+    try:
+        data = child.findtext('raw_text')
+        obsArray = data.split(" ")
+    except:
+        obsArray = None
+    try:
+        temp = float(child.findtext('temp_c'))
+    except:
+        temp = np.nan
+    try:
+        dewp = float(child.findtext('dewpoint_c'))
+    except:
+        dewp = np.nan
+    try:
+        wdir = float(child.findtext('wind_dir_degrees'))
+    except:
+        wdir = np.nan
+    try:
+        wspd = float(child.findtext('wind_speed_kt'))
+    except:
+        wspd = np.nan
+    try:
+        gust = float(child.findtext('wind_gust_kt'))
+    except:
+        gust = np.nan
+    try:
+        precip = float(child.findtext('pcp1hr_in'))
+    except:
+        precip = 0.0
+    try:
+        visby = float(child.findtext('visibility_statute_mi'))
+    except:
+        visby = np.nan
+    try:
+        pres = float(child.findtext('altim_in_hg')) / 0.029530
+    except:
+        pres = np.nan
+    try:
+        wxstring = child.findtext('wx_string')
+    except:
+        wxstring = None
+    try:
+        cloudcovers = []
+        ceilings = []
+        clouds = child.findall('sky_condition')
+        for cloud in clouds:
+            cloudcovers.append(cloud.attrib['sky_cover'])
+            ceilings.append(cloud.attrib['cloud_base_ft_agl'])
+    except:
+        cloudcovers = [None]
+        ceilings = [None]
+    # Done getting data, now pass it along to the METAR object
+    metar.addRawObservation(obsArray)
+    metar.addTemp(temp)
+    metar.addDewp(dewp)
+    metar.addWdir(wdir)
+    metar.addWx(wxstring)
+    metar.addTime(time)
+    metar.addPres(pres)
+    metar.addVis(visby)
+    metar.addWspd(wspd)
+    metar.addGust(gust)
+    metar.addCloudCover(cloudcovers)
+    metar.addCeilings(ceilings)
+    metar.addPrecip(precip)
 
 
 def bulkMetars():
@@ -99,58 +171,9 @@ def bulkMetars():
             if "M" in time:
                 pass
             else:
-
-                try:
-                    data = child.findtext('raw_text')
-                    obsArray = data.split(" ")
-                except:
-                    obsArray = None
-                try:
-                    temp = float(child.findtext('temp_c'))
-                except:
-                    temp = 9999.0
-                try:
-                    dewp = float(child.findtext('dewpoint_c'))
-                except:
-                    dewp = 9999.0
-                try:
-                    wdir = child.findtext('wind_dir_degrees')
-                except:
-                    wdir = None
-                try:
-                    wspd = float(child.findtext('wind_speed_kt'))
-                except:
-                    wspd = 9999.0
-                try:
-                    gust = float(child.findtext('wind_gust_kt'))
-                except:
-                    gust = 9999.0
-                try:
-                    visby = float(child.findtext('visibility_statute_mi'))
-                except:
-                    visby = 9999.0
-                try:
-                    pres = float(child.findtext('sea_level_pressure_mb'))
-                except:
-                    pres = 9999.0
-                try:
-                    wxstring = child.findtext('wx_string')
-                except:
-                    wxstring = None
-                # Done getting data, now pass it along to the METAR object
-                metar = METAR(siteID,lat,lon)
-                metar.addRawObservation(obsArray)
-                metar.addTemp(temp)
-                metar.addDewp(dewp)
-                metar.addWdir(wdir)
-                metar.addWx(wxstring)
-                metar.addTime(time)
-                metar.addPres(pres)
-                metar.addVis(visby)
-                metar.addWspd(wspd)
-                metar.addGust(gust)
+                metar = METAR(siteID, lat, lon)
+                parseAWCxml(metar,child)
                 obs_list.append(metar)
-                print(metar)
         except:
             pass
     return obs_list
@@ -167,91 +190,92 @@ def singleMetar(site,numHours):
     Purpose: This function will use the AWC text dataserver to retrieve all recent METAR
              observations from a single site from (numHours) hours ago.
     """
-    obs_list = []
+    # Do a quick check to see if the call has requested only the most recent obs (numHours = 0)
+    # if so, set lastOnly to true, but get the last hour's worth of obs
+    if numHours == 0:
+        numHours = 1
+        lastOnly = True
+    else:
+        lastOnly = False
     url = "https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString="+site.upper()+"&hoursBeforeNow="+str(numHours)
-    print(url)
     response = requests.get(url)
     tree = ET.fromstring(response.content)
     data = tree[6]
+    # Check again to see if we only want the most recent observation
+    # if so, then set end to 1 to get the latest ob. Otherwise return all obs.
+    if lastOnly:
+        end = 1
+    else:
+        end = len(data)
     # First check to make sure there are valid observations
     try:
         siteID = data[0].findtext('station_id')
         lat = float(data[0].findtext('latitude'))
         lon = float(data[0].findtext('longitude'))
         metar = METAR(siteID, lat, lon)
-        for child in reversed(data): # going in reverse so we add data in chronological order
+        for child in reversed(data[:end]): # going in reverse so we add data in chronological order
                 # Check to make sure there is a valid observation for this time
                 time = child.findtext('observation_time')
                 if "M" in time:
                     pass
                 else:
-
-                    try:
-                        data = child.findtext('raw_text')
-                        obsArray = data.split(" ")
-                    except:
-                        obsArray = None
-                    try:
-                        temp = float(child.findtext('temp_c'))
-                    except:
-                        temp = 9999.0
-                    try:
-                        dewp = float(child.findtext('dewpoint_c'))
-                    except:
-                        dewp = 9999.0
-                    try:
-                        wdir = child.findtext('wind_dir_degrees')
-                    except:
-                        wdir = None
-                    try:
-                        wspd = float(child.findtext('wind_speed_kt'))
-                    except:
-                        wspd = 9999.0
-                    try:
-                        gust = float(child.findtext('wind_gust_kt'))
-                    except:
-                        gust = 9999.0
-                    try:
-                        visby = float(child.findtext('visibility_statute_mi'))
-                    except:
-                        visby = 9999.0
-                    try:
-                        pres = float(child.findtext('sea_level_pressure_mb'))
-                    except:
-                        pres = 9999.0
-                    try:
-                        wxstring = child.findtext('wx_string')
-                    except:
-                        wxstring = None
-                    # Done getting data, now pass it along to the METAR object
-                    metar.addRawObservation(obsArray)
-                    metar.addTemp(temp)
-                    metar.addDewp(dewp)
-                    metar.addWdir(wdir)
-                    metar.addWx(wxstring)
-                    metar.addTime(time)
-                    metar.addPres(pres)
-                    metar.addVis(visby)
-                    metar.addWspd(wspd)
-                    metar.addGust(gust)
+                    parseAWCxml(metar,child)
     except:
         pass
     return metar
 
 
 
+def multipleMetars(siteList):
+    obs_list = []
 
-metars = singleMetar("kokc",4)
+    for site in siteList:
+        if siteList.index(site) == 0:
+            siteString = site.upper()
+        else:
+            siteString = siteString+","+site.upper()
 
-print(metars.times)
-print(metars.temps)
-print(metars.dewps)
+    url = "https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&hoursBeforeNow=1&stationString=" + siteString
+    #works up to here!
+    response = requests.get(url)
+    tree = ET.fromstring(response.content)
+    data = tree[6]
+    for child in data:
+        # First check to make sure there is a valid observations
+        try:
+            siteID = child.findtext('station_id')
+            lat = float(child.findtext('latitude'))
+            lon = float(child.findtext('longitude'))
+            time = child.findtext('observation_time')
+            if "M" in time:
+                pass
+            else:
+                if len(obs_list) > 0 and siteID == obs_list[-1].siteID:
+                    pass
+                else:
+                    metar = METAR(siteID, lat, lon)
+                    parseAWCxml(metar,child)
+                    obs_list.append(metar)
+        except:
+            pass
+    return obs_list
 
-#obs_list = bulkMetars()
+########################################################################################3
 
 
+#metars = singleMetar("kokc",4)
 
-end = time.time()
-print("")
-print("Elapsed Time:")
-print(end-start)
+#print(metars)
+#print(metars.times)
+#print(metars.dewps)
+
+# obs_list = multipleMetars(sites)
+# for ob in obs_list:
+#     print(ob)
+
+
+#
+# end = time.time()
+# print("")
+# print("Elapsed Time:")
+# print(end-start)

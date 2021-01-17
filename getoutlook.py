44cc829245994772d42899ef195286583b9a0c81
admoorewx <41948559+admoorewx@users.noreply.github.com>
2021-01-16 20:16:39 -0600

    Outlook Adjustments
    - Figured out a possible solution to change the projections using QGIS. Required changing how both scripts run though. NBD.

--- getoutlook.py
+++ getoutlook.py
@@ -4,8 +4,14 @@ import zipfile
 
 
 # Laptop
-DataDirectory = 'C:\\Users\\admoo\\Desktop\\Projects\\FalseAlarm\\csv\\'
-outdir = 'C:\\Users\\admoo\\Desktop\\Projects\\FalseAlarm\\outlooks\\'
+# DataDirectory = 'C:\\Users\\admoo\\Desktop\\Projects\\FalseAlarm\\csv\\'
+# outdir = 'C:\\Users\\admoo\\Desktop\\Projects\\FalseAlarm\\outlooks\\'
+# WARfilename = 'ConWarnings_2015_2018.csv'
+
+# home PC
+DataDirectory = 'H:\\Research\\FalseAlarm\\csv\\'
+outdir = 'H:\\Research\\FalseAlarm\\outlooks\\shapefiles\\'
+indir = 'H:\\Research\\FalseAlarm\\outlooks\\'
 WARfilename = 'ConWarnings_2015_2018.csv'
 
 def getDates(DataDirectory,filename):
@@ -23,26 +29,27 @@ def getDates(DataDirectory,filename):
     return output
 
 
-def getOutlook(date,outdir):
+def getOutlook(date,indir):
     year = date[0:4]
     url = "https://www.spc.noaa.gov/products/outlook/archive/"+year+"/day1otlk_"+date+"_1300-shp.zip"
     savename = "day1otlk_"+date+"_1300-shp.zip"
     r = requests.get(url)
-    with open(outdir+savename,'wb') as f:
+    with open(indir+savename,'wb') as f:
         f.write(r.content)
     print("Retrieved file "+savename)
     return savename
 
 
-def unzip(zfile,datadir):
-    path = datadir+zfile[0:-8]
-    os.mkdir(path)
-    with zipfile.ZipFile(datadir+zfile,'r') as zip_ref:
-        zip_ref.extractall(path)
+def unzip(zfile,indir,outdir):
+    inpath = indir+zfile[0:-8]
+    outpath = outdir
+    #os.mkdir(inpath)
+    with zipfile.ZipFile(indir+zfile,'r') as zip_ref:
+        zip_ref.extractall(outpath)
     print("Unzipped file "+zfile+"\n")
 
 
-# output = getDates(DataDirectory,WARfilename)
-# for date in output[0:2]:
-#     filename = getOutlook(date,outdir)
-#     unzip(filename,outdir)
+output = getDates(DataDirectory,WARfilename)
+for date in output[0:3]:
+    filename = getOutlook(date,indir)
+    unzip(filename,indir,outdir)
--- owc.py
+++ owc.py
@@ -1,6 +1,7 @@
-from getoutlook import getDates
-import fiona
+#from getoutlook import getDates
+import fiona, os
 import matplotlib.pyplot as plt
+import cartopy
 import cartopy.crs as ccrs
 from cartopy.io.shapereader import Reader
 from cartopy.feature import ShapelyFeature
@@ -8,11 +9,32 @@ from cartopy.feature import ShapelyFeature
 from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
 
+"""
+Notes:
+        - The "DN" attribute in each of the outlook shapefiles refers to the layer's severe category. 
+          As far as I can tell, the order goes: 
+                                        DN 2 = General Thunder
+                                        DN 3 = Marginal
+                                        DN 4 = Slight
+                                        DN 5 = Enhanced
+                                        DN 6 = Moderate
+                                        DN 7 = High
+        - Note that there is sometimes a second DN 2 layer that has like  points. I'm not sure what this is, but it 
+          is not the actual general thunder area. 
+"""
+
 # Laptop
-DataDirectory = 'C:\\Users\\admoo\\Desktop\\Projects\\FalseAlarm\\csv\\'
-Shapefiles = 'C:\\Users\\admoo\\Desktop\\Projects\\FalseAlarm\\outlooks\\'
-outdir = 'C:\\Users\\admoo\\Desktop\\Projects\\FalseAlarm\\outlooks\\'
+# DataDirectory = 'C:\\Users\\admoo\\Desktop\\Projects\\FalseAlarm\\csv\\'
+# Shapefiles = 'C:\\Users\\admoo\\Desktop\\Projects\\FalseAlarm\\outlooks\\'
+# outdir = 'C:\\Users\\admoo\\Desktop\\Projects\\FalseAlarm\\outlooks\\'
+# WARfilename = 'ConWarnings_2015_2018.csv'
+
+# home PC
+DataDirectory = 'H:\\Research\\FalseAlarm\\csv\\'
+Shapefiles = 'H:\\Research\\FalseAlarm\\outlooks\\day1otlk_20160614_1300\\'
+outdir = 'H:\\Research\\FalseAlarm\\outlooks\\'
 WARfilename = 'ConWarnings_2015_2018.csv'
+
 
 testdates = ["20160517","20170314"]
 
@@ -21,10 +43,12 @@ def plotShapefile(shpfile):
     ax = plt.axes(projection=ccrs.PlateCarree())
     shape_feature = ShapelyFeature(Reader(shpfile).geometries(),
                                    ccrs.PlateCarree(),
-                                   facecolor='k',
-                                   edgecolor='k')
+                                   facecolor='none',
+                                   edgecolor='b')
     ax.add_feature(shape_feature)
     ax.coastlines()
+    ax.add_feature(cartopy.feature.BORDERS)
+    ax.add_feature(cartopy.feature.STATES)
     ax.set_extent([-125,-60,20,50])
     plt.show()
     # One trick may be to convert the current shape file to a different
@@ -35,18 +59,17 @@ def readShapefile(shpfile):
     print("")
     shape = fiona.open(shpfile)
     #print(shape.schema)
-    first = next(iter(shape))
-    for key,value in first.items():
-        #print(value)
-        try:
-            for k,v in value.items():
-                print(k,v)
-        except:
-            continue
+    for feat in shape:
+        #print(feat)
+        print(feat['properties']['DN'])
+        print(len(feat['geometry']['coordinates'][0]))
+        print("")
     plotShapefile(shpfile)
 
 
-for date in testdates:
-    shapefile_dir = "day1otlk_" + date + "_1300\\"
-    shpfile = "day1otlk_" + date + "_1300_hail.shp"
-    readShapefile(Shapefiles+shapefile_dir+shpfile)
+# for date in testdates:
+#     shapefile_dir = "day1otlk_" + date + "_1300\\"
+#     shpfile = "day1otlk_" + date + "_1300_hail.shp"
+#     readShapefile(Shapefiles+shapefile_dir+shpfile)
+
+readShapefile(Shapefiles+"day1otlk_20160614_1300_cat.shp")
